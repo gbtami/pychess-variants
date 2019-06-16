a5b1fe85f56c1b911b575d7c0becf09dd78b6aa5
gbtami <gbtami@gmail.com>
2019-06-16 13:25:38 +0200

    Fix clock box shadow

--- src/clock.ts
+++ src/clock.ts
@@ -123,9 +123,9 @@ export function renderTime(clock, time) {
 
     const date = new Date(time);
     const millis = date.getUTCMilliseconds();
-    clock.el = patch(clock.el, h('div.clock', [
+    clock.el = patch(clock.el, h('div.clock-wrap', [h('div.clock', [
         h('div.clock.time.min', {class: {running: clock.running, hurry: time < 10000, connecting: clock.connecting}}, parsed.minutes),
         h('div.clock.sep', {class: {running: clock.running, hurry: time < 10000, low: millis < 500, connecting: clock.connecting}} , ':'),
         h('div.clock.time.sec', {class: {running: clock.running, hurry: time < 10000, connecting: clock.connecting}}, parsed.seconds),
-        ]));
+        ])]));
 }
