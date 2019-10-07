6cbb6935d1324c6bdd33945d37ff64d359d3f5af
gbtami <gbtami@gmail.com>
2019-10-07 23:56:44 +0200

    Add Discord button

--- src/lobby.ts
+++ src/lobby.ts
@@ -396,7 +396,7 @@ export function lobbyView(model): VNode[] {
     return [h('aside.sidebar-first', [ h('div.lobbychat#lobbychat') ]),
             h('main.main', [ h('table#seeks', {hook: { insert: (vnode) => runSeeks(vnode, model) } }) ]),
             h('aside.sidebar-second', [ h('ul#seekbuttons') ]),
-            h('under-left', "# of users"),
+            h('under-left', [h('a', {class: {'donate-button': true}, attrs: {href: 'https://discord.gg/Z5p9TbM'}}, 'Find partner on Discord'),]),
             h('under-lobby'),
             h('under-right', [
                 h('a', {
