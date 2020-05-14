e175a92cd5c69d8e8d66fe35665a292594cbef09
gbtami <gbtami@gmail.com>
2020-05-14 18:42:43 +0200

    Add pt html docs files

--- static/docs/md2html.sh
+++ static/docs/md2html.sh
@@ -10,6 +10,10 @@ for f in hu/*.md; do
 showdown makehtml -i "$f" -o "$(basename -- "$f" .md).hu.html" --flavor github
 done
 
+for f in pt/*.md; do
+showdown makehtml -i "$f" -o "$(basename -- "$f" .md).pt.html" --flavor github
+done
+
 showdown makehtml -i "zh/intro.md" -o "intro.zh.html" --flavor github
 showdown makehtml -i "fr/intro.md" -o "intro.fr.html" --flavor github
 
