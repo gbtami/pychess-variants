6b0fbc80cd8767997a78104feff5c62e3eddfb40
gbtami <gbtami@gmail.com>
2020-07-10 10:02:54 +0200

    Add blackdisc synochess set

---
+++ static/images/pieces/synochess/xq/create_links.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s ../../merida/wP.svg wP.svg
+ln -s ../../merida/wN.svg wN.svg
+ln -s ../../merida/wB.svg wB.svg
+ln -s ../../merida/wR.svg wR.svg
+ln -s ../../merida/wQ.svg wQ.svg
+ln -s ../../merida/wK.svg wK.svg
