d6d7d8343acd474f04126470e427c4af268a6918
Ada Joule <ada.fulmina@gmail.com>
2020-07-11 14:37:11 +0700

    Format code in png.ts

--- client/png.ts
+++ client/png.ts
@@ -6,8 +6,8 @@ export function copyBoardToPNG(fen) {
     const width = parseInt(style.getPropertyValue('--cgwrapwidth'));
     const height = parseInt(style.getPropertyValue('--cgwrapheight'));
     toPng(el, {width: width, height: height})
-        .then(function (dataUrl) {
-            let link = document.createElement('a');
+        .then(dataUrl => {
+            const link = document.createElement('a');
             link.download = fen.split(' ')[0].replace(/\+/g, '.') + '.png';
             link.href = dataUrl;
             link.click();
