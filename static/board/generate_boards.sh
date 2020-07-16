9fc60c18f8a95a9982f87be07cdadccf05605ffd
Ada Joule <ada.fulmina@gmail.com>
2020-07-17 06:36:37 +0700

    Add board style CSS generator script

---
+++ static/board/generate_boards.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sh gencss.sh makruk8x8 "makruk2.svg" "makruk.svg" "makruk.jpg"
+sh gencss.sh sittuyin8x8 "sittuyin.svg" "sittuyin.jpg" "sittuyingreen.svg" "sittuyinGrainBrown.svg"
+sh gencss.sh shogi9x9 "shogi.svg" "Shogiban1.png" "Shogiban2.png" "shogic.svg" "ShogiMaple.png" "doubutsu.svg"
+sh gencss.sh shogi5x5 "minishogi.svg" "MiniboardWood1.png" "MiniboardWood2.png"
+sh gencss.sh janggi9x10 "Janggi.svg" "JanggiPaper.png" "JanggiWood.png" "JanggiDark.svg" "JanggiBrown.svg"
+sh gencss.sh xiangqi9x10 "xiangqi.svg" "xiangqic.svg" "xiangqiCTexture.png" "xiangqiPaper.png" "xiangqiWood.png" "xiangqiDark.svg"
+sh gencss.sh xiangqi7x7 "minixiangqi.svg" "minixiangqiw.png" "minixqlg.svg"
+sh gencss.sh standard8x8 "8x8brown.svg" "8x8blue.svg" "8x8green.svg" "8x8maple.jpg" "8x8olive.jpg"
+sh gencss.sh standard10x8 "10x8brown.svg" "10x8blue.svg" "10x8green.svg" "10x8maple.jpg" "10x8olive.jpg"
+sh gencss.sh standard10x10 "10x10brown.svg" "10x10blue.svg" "10x10green.svg" "10x10maple.jpg" "10x10olive.jpg"
+sh gencss.sh grand10x10 "Grandboard.svg" "10x10brown.svg" "10x10blue.svg" "10x10green.svg" "10x10maple.jpg" "10x10mapleGrand.png"
+sh gencss.sh shogun8x8 "ShogunPlain.svg" "ShogunMaple.png" "ShogunMaple2.png" "ShogunBlue.svg" "8x8brown.svg" "8x8maple.jpg"
