00c3b8e386bfa600928986146d2e65ccd26f9aeb
gbtami <gbtami@gmail.com>
2020-09-24 12:25:48 +0200

    Fixed Dobutsu piece guide markers (arrows -> dots)

--- static/images/pieces/shogi/Ka/dobutsu/create_dobutsu_links.sh
+++ static/images/pieces/shogi/Ka/dobutsu/create_dobutsu_links.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# chick
+ln -s ../0FU.svg 0FU.svg
+ln -s ../1FU.svg 1FU.svg
+
+# hen
+ln -s ../0TO.svg 0TO.svg
+ln -s ../1TO.svg 1TO.svg
+
+# lion
+ln -s ../0GY.svg 0GY.svg
+ln -s ../1GY.svg 1GY.svg
+
+# analysis drop suggestion images
 ln -s ../1FU.svg bC.svg
-ln -s ../1HI.svg bG.svg
-ln -s ../1KA.svg bE.svg
+ln -s 1HI.svg bG.svg
+ln -s 1KA.svg bE.svg
 
 ln -s ../0FU.svg wC.svg
-ln -s ../0HI.svg wG.svg
-ln -s ../0KA.svg wE.svg
+ln -s 0HI.svg wG.svg
+ln -s 0KA.svg wE.svg
