e53f884ccb849ece65b3dbeedb93f7a65705d0f5
gbtami <gbtami@gmail.com>
2020-11-21 20:04:26 +0100

    Invite page

---
+++ client/invite.ts
@@ -0,0 +1,45 @@
+import h from 'snabbdom/h';
+import { VNode } from 'snabbdom/vnode';
+
+import { _ } from './i18n';
+import { VARIANTS } from './chess';
+import { gameType } from './profile';
+import { copyTextToClipboard } from './clipboard';
+
+export function inviteView(model): VNode[] {
+    const gameId = model["gameId"];
+    const variant = VARIANTS[model.variant];
+    const chess960 = model.chess960 === 'True';
+    const gameURL = model["home"] + '/' + gameId;
+
+    const evtSource = new EventSource("/api/invites");
+    evtSource.onmessage = function(event) {
+        const message = JSON.parse(event.data);
+        if (message.gameId === gameId) {
+            window.location.assign(gameURL);
+        }
+    }
+
+    return [
+        h('div.invite', [
+            h('h1', { attrs: { align: 'center' } }, _('Challenge to a game')),
+            h('div.invitegame', [
+                h('div.info0.games.icon', { attrs: { "data-icon": variant.icon(chess960) } }, [
+                    h('div.info2', [
+                        h('div', variant.displayName(chess960)),
+                        h('div.tc', model["base"] + "+" + (model["byo"] > 1 ? model["byo"] + "x" : "") + model["inc"] + (model["byo"] > 0 ? "(b)" : "")),
+                    ]),
+                    h('div.rated', gameType(model["rated"])),
+                ]),
+            ]),
+            h('div.inviteinfo', [
+                h('div', _('To invite someone to play, give this URL:')),
+                h('input#invite-url', {attrs: {readonly: true, spellcheck: false, value: gameURL}}),
+                h('button#paste-url', { on: { click: () => copyTextToClipboard(gameURL) } }, [
+                    h('i', {props: {title: _('Copy URL')}, class: {"icon": true, "icon-clipboard": true} })]),
+                h('div', _('The first person to come to this URL will play with you.')),
+
+            ]),
+        ]),
+    ];
+}
