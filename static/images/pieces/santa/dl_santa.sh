97a2b6898857ca05d0a9fbf7f20cd86959895406
gbtami <gbtami@gmail.com>
2020-12-23 15:10:53 +0100

    Santa theme

---
+++ static/images/pieces/santa/dl_santa.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget -O wP.png https://i.imgur.com/p2VBPkD.png
+wget -O wN.png https://i.imgur.com/sVNcfld.png
+wget -O wB.png https://i.imgur.com/JKFEE0N.png
+wget -O wR.png https://i.imgur.com/LTkBSOf.png
+wget -O wQ.png https://i.imgur.com/GpdmACB.png
+wget -O wK.png https://i.imgur.com/ItJll7K.png
+
+wget -O bP.png https://i.imgur.com/czB0T91.png
+wget -O bN.png https://i.imgur.com/AsnR9mS.png
+wget -O bB.png https://i.imgur.com/OT1fRvq.png
+wget -O bR.png https://i.imgur.com/VklhtC0.png
+wget -O bQ.png https://i.imgur.com/QRW5SHt.png
+wget -O bK.png https://i.imgur.com/YTBxYBe.png
+
+wget -O 8x8santa.png http://i.imgur.com/1sK5kku.png
