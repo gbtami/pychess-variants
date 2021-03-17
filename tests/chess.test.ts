167e6f91d3704976afc8e7cb7823155fa51108c1
gbtami <gbtami@gmail.com>
2021-03-17 15:47:02 +0100

    Add another rivial unit test

--- tests/chess.test.ts
+++ tests/chess.test.ts
@@ -1,4 +1,4 @@
-import { VARIANTS, getPockets, isHandicap, validFen } from '../client/chess';
+import { VARIANTS, getPockets, isHandicap, validFen, cg2uci, uci2cg } from '../client/chess';
 import { expect } from 'chai';
 
 describe('getPockets test', 
@@ -31,5 +31,21 @@ describe('validFen test',
             const variant = VARIANTS['chess'];
             const result = validFen(variant, variant.startFen);
             expect(result).to.be.true; 
+    }); 
+});
+
+describe('uci2cg test', 
+    () => { 
+        it('should chnage all occurences of "10" to ":" in UCI moves', () => { 
+            const result = uci2cg('a10j10');
+            expect(result).to.be.equal('a:j:'); 
+    }); 
+});
+
+describe('cg2uci test', 
+    () => { 
+        it('should chnage all occurences of ":" to "10" in UCI moves', () => { 
+            const result = cg2uci('a:j:');
+            expect(result).to.be.equal('a10j10'); 
     }); 
 });
