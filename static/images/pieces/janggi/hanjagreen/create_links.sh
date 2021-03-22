ff42e3f21d9e24ea78b6109eba65fd0f90286e22
gbtami <gbtami@gmail.com>
2021-03-22 13:35:33 +0100

    Remove Janggi piece symlinks

--- static/images/pieces/janggi/hanjagreen/create_links.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ln -s ../hanjablue/red_advisor.svg red_advisor.svg
-ln -s ../hanjablue/red_cannon.svg red_cannon.svg
-ln -s ../hanjablue/red_chariot.svg red_chariot.svg
-ln -s ../hanjablue/red_elephant.svg red_elephant.svg
-ln -s ../hanjablue/red_horse.svg red_horse.svg
-ln -s ../hanjablue/red_king.svg red_king.svg
-ln -s ../hanjablue/red_pawn.svg red_pawn.svg
