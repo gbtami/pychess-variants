93a0e9bd696e7c15ee432dc207907ed7b96980f5
gbtami <gbtami@gmail.com>
2021-03-22 11:39:01 +0100

    Remove Synochess piece symlinks

--- static/images/pieces/synochess/blackdisc/create_links.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -s ../../merida/wP.svg wP.svg
-ln -s ../../merida/wN.svg wN.svg
-ln -s ../../merida/wB.svg wB.svg
-ln -s ../../merida/wR.svg wR.svg
-ln -s ../../merida/wQ.svg wQ.svg
-ln -s ../../merida/wK.svg wK.svg
