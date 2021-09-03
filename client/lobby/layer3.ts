c307223b224426485494cd1734d33b737cfd3832
gbtami <gbtami@gmail.com>
2021-09-04 00:23:57 +0200

    Layer3 pro-tip text

--- client/lobby/layer3.ts
+++ client/lobby/layer3.ts
@@ -38,9 +38,9 @@ export function layer3variant (containerId: string, model: string, variantName: 
     }
 
     const layer3cont = h('div#chessl3cont.layer-3-container.chess-l3', [
-        h('button.layer-2-category lvl3', [
+        h('button.layer-2-category l3v', [
             h('div.variant-title-l2', [
-                h('div.iconcontainer.icon', { attrs: { 'data-icon': variant.icon(chess960) } }, variant.displayName(chess960)),
+                h('div.icon', { attrs: { 'data-icon': variant.icon(chess960) } }, variant.displayName(chess960)),
             ]),
             h('ul.l3links-cont', [
                 h('li.l3links', 'Create a game'),
@@ -56,7 +56,7 @@ export function layer3variant (containerId: string, model: string, variantName: 
         h('button.layer-2-category l3t', [
             h('p.variant-extra-info', variant.tooltip()),
             h('a.variant-extra-info', { attrs: { href: model['home'] + '/variants/' + variant.name, target: '_blank' } }, 'Rules'),
-            h('p.variant-extra-info', 'Tip: You can play more at www.lichess.org'),
+            h('p.variant-extra-info', 'Tip: ' + proTip(variant.name, chess960)),
         ]),
     ]);
 
@@ -64,37 +64,66 @@ export function layer3variant (containerId: string, model: string, variantName: 
     if (container) patch(container, layer3cont);
 }
 
-/*
-            <div class="layer-3-container chess-l3 remove-btn" id="chessl3cont">
-
-                <button class="layer-2-category l3v">
-
-                    <div class="variant-title-l2"> <div class="icon-container"> <img src="/images/icons/chess.svg" alt=""></div> <h3>Chess</h3></div>
-
-                    <ul class="l3links-cont">
-                     <li class="l3links"><a href="">Create a game</a></li>
-                     <li class="l3links"><a href="">Challenge a friend</a></li>
-                     <li class="l3links"><a href="">Play against AI</a></li>
-                     <li class="l3links"><a href="">Play against RM</a></li>
-                     <li class="l3links"><a href="">Analyze</a></li>
-                    </ul>
-
-                    <h5 id="chessl3back"> Go Back</h5>
-                </button>
-
-                <button class="layer-2-category l3img">
-                    <div class="generic-image-container generic-shogi"><img src="/images/Chess.png" alt=""></div>
-                </button>
-
-                <button class="layer-2-category l3t">
-                <p class="variant-extra-info">Western or International Chess, unmodified, as it's played by FIDE standards.
-                </p>
-                <a class="variant-extra-info" href="https://www.pychess.org/variants/chess" target="_blank">Rules
-                </a>
-                <p class="variant-extra-info">Tip: You can play more at www.lichess.org
-                </p>
-
-                </button>
-
-            </div>
-*/
+function proTip (variant: string, chess960: boolean) {
+    switch (variant) {
+// chess
+    case 'chess':
+    case 'crazyhouse':
+    case 'atomic':
+        if (chess960) {
+            return 'Move the king on top of the rook to castle.';
+        } else {
+            return 'You can play more at www.lichess.org';
+        }
+    case 'placement':
+        return 'Castling only possible if king and rook are dropped to their usual places like in standard chess.';
+// fairy
+    case 'capablanca':
+        return 'You can choose different starting setups including Embassy Chess and Gothic Chess. For a drop (+/- 960) version, choose CAPAHOUSE from the dropdown menu.';
+    case 'grand':
+        return 'For a drop version, choose GRANDHOUSE from the dropdown menu.';
+    case 'seirawan':
+        return 'For a drop version, choose S-HOUSE from the dropdown menu.';
+    case 'shogun':
+        return 'The Queen actually demotes upon capture, so it is not worth as much.';
+    case 'shako':
+        return 'The cannon is a tricky piece that needs to be closely followed.';
+    case 'hoppelpoppel':
+        return 'Other piece sets are available, which may help.';
+// army
+    case 'orda':
+    case 'shinobi':
+    case 'empire':
+    case 'synochess':
+    case 'ordamirror':
+        return 'Be aware of campmate - victory by moving your king into the 8th rank.';
+// makruk
+    case 'makruk':
+        return '';
+    case 'makpong':
+        return 'Watch out for knight checks, since they cannot be blocked.';
+    case 'sittuyin':
+        return 'Be sure to review the nuances of pawn promotion rules.';
+    case 'cambodian':
+        return '';
+// shogi
+    case 'shogi':
+    case 'minishogi':
+    case 'gorogoro':
+        return 'Internationalized sets are available by going to settings.';
+    case 'dobutsu':
+        return 'Despite the simple appearance, there is still quite a bit of depth underlying this game.';
+    case 'kyotoshogi':
+        return 'Sets that show both sides as available by going to settings.';
+    case 'torishogi':
+        return 'Reading the rules are a necessity to understanding the movements before playing.';
+// xiangqi
+    case 'xiangqi':
+    case 'janggi':
+    case 'minixiangqi':
+    case 'manchu':
+        return 'Internationalized sets are available by going to settings.';
+    default:
+        return '';
+    }
+}
