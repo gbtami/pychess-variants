b56e3d4516de34ba91e63ee08ee9b6d507d6faa4
Ada Joule <ada.fulmina@gmail.com>
2021-10-06 18:43:52 +0700

    Add brotli to prod config

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,6 +2,8 @@ import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { wasm } from '@rollup/plugin-wasm';
+import brotli from 'rollup-plugin-brotli';
+import zlib from 'zlib';
 
 export default {
     input: "client/main.ts",
@@ -10,5 +12,15 @@ export default {
         file: "static/pychess-variants.js",
         format: "iife",
     },
-    plugins: [nodeResolve(), commonjs(), typescript(), wasm()],
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        typescript(),
+        wasm(),
+        brotli({
+            params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+            },
+        }),
+    ],
 }
