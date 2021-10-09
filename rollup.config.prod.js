67f483a932e37abb8d28544d47fae8c8a0ee62df
gbtami <gbtami@gmail.com>
2021-10-10 01:56:38 +0200

    Remove unused import

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { wasm } from '@rollup/plugin-wasm';
 import brotli from 'rollup-plugin-brotli';
 import zlib from 'zlib';
 
