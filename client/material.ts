6c8040bad7394a9895d00fe8f0f25e017fc3697c
Konrad Paluszek <kezsulap@gmail.com>
2021-10-09 19:56:48 +0200

    Fix type warnings in material.ts

--- client/material.ts
+++ client/material.ts
@@ -31,13 +31,13 @@ function mapPiece(piece: string, variant: string): string {
 }
 
 function calculateImbalance(ctrl: RoundController): {[index: string]:number} {
-    var imbalances : {[index: string]:number} = {};
-    var topMaterialColor = ctrl.flip ? ctrl.mycolor : ctrl.oppcolor, bottomMaterialColor = ctrl.flip ? ctrl.oppcolor : ctrl.mycolor;
-    for (var piece of ctrl.variant.pieceRoles('white')) imbalances[mapPiece(piece, ctrl.variant.name)] = 0;
-    for (var piece of ctrl.variant.pieceRoles('black')) imbalances[mapPiece(piece, ctrl.variant.name)] = 0;
-    for (var coord of Object.keys(ctrl.chessground.state.pieces)) {
-        var pieceObject = ctrl.chessground.state.pieces[coord];
-        var mappedPiece = mapPiece(pieceObject!.role, ctrl.variant.name);
+    let imbalances : {[index: string]:number} = {};
+    let topMaterialColor = ctrl.flip ? ctrl.mycolor : ctrl.oppcolor, bottomMaterialColor = ctrl.flip ? ctrl.oppcolor : ctrl.mycolor;
+    for (let piece of ctrl.variant.pieceRoles('white')) imbalances[mapPiece(piece, ctrl.variant.name)] = 0;
+    for (let piece of ctrl.variant.pieceRoles('black')) imbalances[mapPiece(piece, ctrl.variant.name)] = 0;
+    for (let piece of ctrl.chessground.state.pieces) {
+        let pieceObject = piece[1];
+        let mappedPiece = mapPiece(pieceObject!.role, ctrl.variant.name);
         if (pieceObject!.color == 'white') {
             imbalances[mappedPiece]++;
         }
@@ -46,10 +46,10 @@ function calculateImbalance(ctrl: RoundController): {[index: string]:number} {
         }
     }
     if (ctrl.hasPockets) {
-        for (var piece in ctrl.pockets[0]) {
+        for (let piece in ctrl.pockets[0]) {
             imbalances[mapPiece(piece, ctrl.variant.name)] += (topMaterialColor === 'white' ? 1 : -1) * ctrl.pockets[0][piece as cg.Role]!;
         }
-        for (var piece in ctrl.pockets[1]) {
+        for (let piece in ctrl.pockets[1]) {
             imbalances[mapPiece(piece, ctrl.variant.name)] += (bottomMaterialColor === 'white' ? 1 : -1) * ctrl.pockets[1][piece as cg.Role]!;
         }
     }
@@ -57,14 +57,14 @@ function calculateImbalance(ctrl: RoundController): {[index: string]:number} {
 }
 
 function generateContent(ctrl: RoundController, imbalances: {[index: string]:number}, color: cg.Color): VNode[] {
-    var result : VNode[] = [];
-    for (var piece of ctrl.variant.pieceRoles(color)) {
-        var mappedPiece = mapPiece(piece, ctrl.variant.name);
-        var difference = imbalances[mappedPiece] * (color === 'white' ? 1 : -1);
+    let result : VNode[] = [];
+    for (let piece of ctrl.variant.pieceRoles(color)) {
+        let mappedPiece = mapPiece(piece, ctrl.variant.name);
+        let difference = imbalances[mappedPiece] * (color === 'white' ? 1 : -1);
         if (difference > 0) {
             imbalances[mappedPiece] = 0;
-            var current_div : VNode[] = [];
-            for (var i = 0; i < difference; ++i) {
+            let current_div : VNode[] = [];
+            for (let i = 0; i < difference; ++i) {
                 current_div.push(h('mpiece.' + mappedPiece))
             }
             result.push(h('div', current_div));
@@ -79,14 +79,14 @@ function makeMaterialVNode(ctrl: RoundController, which: string, color: cg.Color
 
 export function updateMaterial (ctrl: RoundController, vmaterial0?: VNode | HTMLElement, vmaterial1?: VNode | HTMLElement) {
     if (!ctrl.variant.materialDifference) return;
-    var topMaterialColor = ctrl.flip ? ctrl.mycolor : ctrl.oppcolor, bottomMaterialColor = ctrl.flip ? ctrl.oppcolor : ctrl.mycolor;
+    let topMaterialColor = ctrl.flip ? ctrl.mycolor : ctrl.oppcolor, bottomMaterialColor = ctrl.flip ? ctrl.oppcolor : ctrl.mycolor;
     if (!ctrl.materialDifference) {
         ctrl.vmaterial0 = patch(vmaterial0? vmaterial0 : ctrl.vmaterial0, makeMaterialVNode(ctrl, 'top', topMaterialColor, []));
         ctrl.vmaterial1 = patch(vmaterial1? vmaterial1 : ctrl.vmaterial1, makeMaterialVNode(ctrl, 'bottom', bottomMaterialColor, []));
         return;
     }
-    var imbalances = calculateImbalance(ctrl);
-    var topMaterialContent = generateContent(ctrl, imbalances, topMaterialColor), bottomMaterialContent = generateContent(ctrl, imbalances, bottomMaterialColor);
+    let imbalances = calculateImbalance(ctrl);
+    let topMaterialContent = generateContent(ctrl, imbalances, topMaterialColor), bottomMaterialContent = generateContent(ctrl, imbalances, bottomMaterialColor);
     ctrl.vmaterial0 = patch(vmaterial0? vmaterial0 : ctrl.vmaterial0, makeMaterialVNode(ctrl, 'top', topMaterialColor, topMaterialContent));
     ctrl.vmaterial1 = patch(vmaterial1? vmaterial1 : ctrl.vmaterial1, makeMaterialVNode(ctrl, 'bottom', bottomMaterialColor, bottomMaterialContent));
 }
