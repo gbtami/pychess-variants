6a18123449e66f73324521b56b77edac19e2984a
Ada Joule <ada.fulmina@gmail.com>
2021-10-10 22:15:45 +0700

    Add type information to chat response

--- server/chat.py
+++ server/chat.py
@@ -1,4 +1,12 @@
-def chat_response(type: str, username: str, message: str, room: str = "") -> dict:
+from typing import TypedDict
+
+class ChatResponse(TypedDict):
+    type: str
+    user: str
+    message: str
+    room: str
+
+def chat_response(type: str, username: str, message: str, room: str = "") -> ChatResponse:
     return {
         "type": type,
         "user": username,
