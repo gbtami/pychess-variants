32ff971d1800727f8494694ae6d82bd9c19a331b
gbtami <gbtami@gmail.com>
2021-12-05 00:23:09 +0100

    Raised high score min games

--- server/const.py
+++ server/const.py
@@ -4,7 +4,7 @@ from settings import static_url
 MAX_CHAT_LINES = 100
 
 # Minimum number of rated games needed
-HIGHSCORE_MIN_GAMES = 5
+HIGHSCORE_MIN_GAMES = 10
 
 # Show the number of spectators only after this limit
 MAX_NAMED_SPECTATORS = 20
