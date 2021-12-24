18d1628615d0a40e8071cf0b8e5ed934c47434a6
gbtami <gbtami@gmail.com>
2021-12-24 12:08:02 +0100

    Fix merge conflict

--- server/const.py
+++ server/const.py
@@ -90,11 +90,7 @@ VARIANTS = (
     "empire",
     "ordamirror",
     "chak",
-<<<<<<< HEAD
     "chennis",
-=======
-    # "chennis"
->>>>>>> 164603ffe611b0eece63eb50f2f26e871eccd475
 )
 
 VARIANT_ICONS = {
