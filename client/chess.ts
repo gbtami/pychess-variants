d9155fc5cd61475c5b7881eefc0643c8a7d9e53d
SriMethan <77402282+SriMethan@users.noreply.github.com>
2022-01-22 12:52:37 +0530

    Enable Antizh

--- client/chess.ts
+++ client/chess.ts
@@ -771,7 +771,7 @@ const disabledVariants = [ "gothic", "gothhouse", "embassy" ];
 export const enabledVariants = variants.filter(v => !disabledVariants.includes(v));
 
 const variantGroups: { [ key: string ]: { variants: string[] } } = {
-    standard: { variants: [ "antichess", "losers", "anti_antichess", "antiatomic"] },
+    standard: { variants: [ "antichess", "losers", "anti_antichess", "antiatomic", "antizh"] },
     //sea:      { variants: [ "makruk", "makpong", "cambodian", "sittuyin", "asean" ] },
     //shogi:    { variants: [ "shogi", "minishogi", "kyotoshogi", "dobutsu", "gorogoro", "torishogi" ] },
     //xiangqi:  { variants: [ "xiangqi", "manchu", "janggi", "minixiangqi" ] },
