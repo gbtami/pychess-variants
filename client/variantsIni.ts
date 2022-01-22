3032052ce674d9b896993b0ad180cd3740d1fd1d
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2022-01-22 12:34:01 +0530

    Fix (#16)

--- client/variantsIni.ts
+++ client/variantsIni.ts
@@ -1,4 +1,9 @@
 export const variantsIni = `
-# Hybrid of antichess and atomic.
+# Lose at anti-chess win at anti-antichess.
+[anti_antichess:giveaway]
+extinctionValue = loss
+stalemateValue = loss
+
+# Hybrid of antichess and atomic
 [antiatomic:giveaway]
 blastOnCapture = true`
