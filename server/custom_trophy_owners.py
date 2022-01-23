cf0b0eb9a4004815b00c5c872b6b36c3afd24676
SriMethan <77402282+SriMethan@users.noreply.github.com>
2022-01-23 16:47:09 +0530

    fix typo

--- server/custom_trophy_owners.py
+++ server/custom_trophy_owners.py
@@ -2,7 +2,7 @@ CUSTOM_TROPHY_OWNERS = {
     # "username": ("variant", "key in TROPHIES")
     # if not for any particular variant put "liantichess".
     "GrohochetGrom": ("antichess", "acwc21"),
-    "SriMethan": ("liantichess", "developer")
+    "SriMethan": ("liantichess", "developer"),
     "SriMethan": ("liantichess", "moderator"),
     "cFlour": ("liantichess", "moderator"),
     "YohaanSethNathan": ("liantichess", "developer")
