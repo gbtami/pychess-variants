a8a06df2b75ef281a193893c6aeb1233ed57a3ac
Hyper-Stockfish <91301864+Hyper-Stockfish@users.noreply.github.com>
2022-02-06 14:51:55 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = base64.urlsafe_b64decode(FERNET_KEY)
 MAX_AGE = 3600 * 24 * 365
 
 MONGO_HOST = os.getenv("MONGO_HOST", "mongodb://127.0.0.1:27017")
-MONGO_DB_NAME = "livariants"
+MONGO_DB_NAME = "pychess-variants"
 
 BOT_TOKENS = json.loads(os.getenv("BOT_TOKENS", "{}"))
 FISHNET_KEYS = json.loads(os.getenv("FISHNET_KEYS", "{}"))
