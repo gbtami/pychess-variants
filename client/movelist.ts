757f4cd6401cc5e31bcd1de888cdd7a252d7ae41
Ada Joule <ada.fulmina@gmail.com>
2022-04-10 05:22:15 +0700

    Show zero move time

--- client/movelist.ts
+++ client/movelist.ts
@@ -82,7 +82,7 @@ export function updateMovelist (ctrl: AnalysisController | RoundController, full
 
         const moveEl = [ h('san', move) ];
         const movetime = ctrl.steps[ply].clocks?.movetime;
-        if (movetime)
+        if (movetime !== undefined)
             // Move time is in milliseconds
             moveEl.push(h('movetime', (movetime / 1000).toFixed(3) + "s"));
         const scoreStr = ctrl.steps[ply]['scoreStr'] ?? '';
