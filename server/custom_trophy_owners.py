938c91cd5445fd1c59c4abbbd412c496a2763568
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2022-04-10 16:31:42 +0530

    Fix custom trophies (#80)

--- server/custom_trophy_owners.py
+++ server/custom_trophy_owners.py
@@ -1,26 +1,62 @@
 CUSTOM_TROPHY_OWNERS = {
     # "username": ("variant", "key in TROPHIES")
     # if not for any particular variant put "liantichess".
-    "GrohochetGrom": ("liantichess", "acwc21"),
+    "GrohochetGrom": (
+        ("liantichess", "acwc21"),
+    ),
     "SriMethan": (
         ("liantichess", "developer"),
         ("liantichess", "moderator"),
     ),
-    "cFlour": ("liantichess", "moderator"),
-    "YohaanSethNathan": ("liantichess", "developer"),
-#    "Kex09": ("liantichess", "marathon1"),
-#    "ElsaSchneider": ("liantichess", "marathon5"),
-#    "ChangeOpening": ("liantichess", "marathon5"),
-#    "Schizophrenic_Energy": ("liantichess", "marathon5"),
-#    "JackDanielsHoney": ("liantichess", "marathon5"),
-#    "Kotov_Syndrome": ("liantichess", "marathon15"),
-#    "Raicosta": ("liantichess", "marathon15"),
-#    "Nikhil_Nikhi": ("liantichess", "marathon15"),
-#    "basschess_1989": ("liantichess", "marathon15"),
-#    "RexIustus": ("liantichess", "marathon15"),
-#    "aleksschtin": ("liantichess", "marathon15"),
-#    "kuhar": ("liantichess", "marathon15"),
-#    "nicolas647": ("liantichess", "marathon15"),
-#    "tolius": ("liantichess", "marathon15"),
-#    "Nefilibata": ("liantichess", "marathon15"),
+    "cFlour": (
+        ("liantichess", "moderator"),
+    ),
+    "YohaanSethNathan": (
+        ("liantichess", "developer"),
+    ),
+    "Kex09": (
+        ("liantichess", "marathon1"),
+    ),
+    "ElsaSchneider": (
+        ("liantichess", "marathon5"),
+    ),
+    "ChangeOpening": (
+        ("liantichess", "marathon5"),
+    ),
+    "Schizophrenic_Energy": (
+        ("liantichess", "marathon5"),
+    ),
+    "JackDanielsHoney": (
+        ("liantichess", "marathon5"),
+    ),
+    "Kotov_Syndrome": (
+        ("liantichess", "marathon15"),
+    ),
+    "Raicosta": (
+        ("liantichess", "marathon15"),
+    ),
+    "Nikhil_Nikhi": (
+        ("liantichess", "marathon15"),
+    ),
+    "basschess_1989": (
+        ("liantichess", "marathon15"),
+    ),
+    "RexIustus": (
+        ("liantichess", "marathon15"),
+    ),
+    "aleksschtin": (
+        ("liantichess", "marathon15"),
+    ),
+    "kuhar": (
+        ("liantichess", "marathon15"),
+    ),
+    "nicolas647": (
+        ("liantichess", "marathon15"),
+    ),
+    "tolius": (
+        ("liantichess", "marathon15"),
+    ),
+    "Nefilibata": (
+        ("liantichess", "marathon15"),
+    ),
 }
