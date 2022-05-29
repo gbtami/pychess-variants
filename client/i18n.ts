8ccd879168ee32950a4d6244bdee282b55443f69
gbtami <gbtami@gmail.com>
2022-05-29 18:15:50 +0200

    Add missin 'Chennis' to translatable variant names

--- client/i18n.ts
+++ client/i18n.ts
@@ -77,7 +77,7 @@ export const translatedVariantDisplayNames = [
     _("capablanca"), _("capahouse"), _("s-chess"), _("s-house"),
     _("grand"), _("grandhouse"), _("shako"), _("shogun"), _("hoppel-poppel"),
 
-    _("orda"), _("synochess"), _("shinobi"), _("empire"), _("orda mirror"), _("chak"),
+    _("orda"), _("synochess"), _("shinobi"), _("empire"), _("orda mirror"), _("chak"), _("chennis"),
 ];
 
 export const translatedCustomStartPositions = [
