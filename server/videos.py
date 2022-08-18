46e1c3c612140404d671d0f4fcde8fb682bbf612
gbtami <gbtami@gmail.com>
2022-08-18 22:04:57 +0200

    Add TODO

--- server/videos.py
+++ server/videos.py
@@ -1,3 +1,8 @@
+# TODO:
+# Add this to mongodb
+# make videos page paginated
+# add tags
+
 VIDEO_TAGS = (
     "How to play",
     "Introduction",
@@ -14,7 +19,10 @@ VIDEO_TAGS = (
 
 VIDEOS = {
     "Vyc4Llxgke8": ("Introduction to the PyChess Website", "20:48"),
-    "WmMw97hp8C0": ("Makpong, Ouk Chatrang (Cambodian Chess), and ASEAN Chess - How to play", "5:26"),
+    "WmMw97hp8C0": (
+        "Makpong, Ouk Chatrang (Cambodian Chess), and ASEAN Chess - How to play",
+        "5:26",
+    ),
     "LWl8nMYrONM": ("How to checkmate with 2 knights (in Hoppel-Poppel)", "13:36"),
     "uyNsTgo8ylI": ("Makruk (Thai Chess) - How to play", "6:19"),
     "p1WEdE3TdM8": ("如果你一直想找個集分析、對奕、競技於一身的網站，那這隻影片將帶給你Pychess這個好地方。", "30:05"),
@@ -22,19 +30,28 @@ VIDEOS = {
     "Ap4mGkR8HDA": ("Orda Chess (and Mirror) - How to play", "11:15"),
     "5f9QKK7cm20": ("Tori Shogi - How to play", "12:14"),
     "YH63AlxpXkg": ("Shogi - How to play (using internationalized piece set)", "17:40"),
-    "KDkF2dEt41g": ("Janggi (Korean Chess) - How to play (using internationalized piece set)", "17:13"),
+    "KDkF2dEt41g": (
+        "Janggi (Korean Chess) - How to play (using internationalized piece set)",
+        "17:13",
+    ),
     "e4jYQ0UMmGk": ("Hybrid Piece Basics", "9:02"),
     "WRk3ZbX2bpA": ("Shogun Chess - How to play", "4:37"),
     "E6AzOO4-340": ("S-Chess (Seiwaran Chess, SHARPER Chess) - How to play", "3:02"),
     "CRrncO-w524": ("Grand Chess - How to play", "2:06"),
     "HNYWioiltH0": ("Capablanca Chess - How to Play", "6:18"),
     "boT1qyDA5RA": ("Xiangqi Basics", "16:53"),
-    "5EDG5RP8OZ8": ("Xiangqi (Chinese Chess) - How to play (Using Internationalized Piece Set)", "8:13"),
+    "5EDG5RP8OZ8": (
+        "Xiangqi (Chinese Chess) - How to play (Using Internationalized Piece Set)",
+        "8:13",
+    ),
     "ujWzsxm18aQ": ("Seirawan-Sharper Chess introduction with GM Yasser and JannLee", "2:27:11"),
     "-b91uceklhM": ("หมากรุกไทย: เว็บใหม่มาเเรง Pychess.org (มีเอไอให้เล่นฟรี)", "15:10"),
     "xw6NpYeuozQ": ("Crazyhouse 960 - opperwezen vs JannLee (Series 3)", "1:50:52"),
     "WCJZj6szAJk": ("Xiangqi opening principles - and why they differ from chess", "32:35"),
-    "-DHY3xhB0aE": ("European Grandmaster Joep Nabuurs' #1 Tip for Chess Players Trying to Improve at Xiangqi", "12:47"),
+    "-DHY3xhB0aE": (
+        "European Grandmaster Joep Nabuurs' #1 Tip for Chess Players Trying to Improve at Xiangqi",
+        "12:47",
+    ),
     "pX_ZDjeqlJs": ("Janggi - basic opening principles", "21:18"),
     "-4ETYXWLEXs": ("Round1", "39:50"),
     "WALKTGnkrYM": ("Round2", "32:13"),
