db958d426d0a5dd0954ce31c9208e273a56a4b6e
gbtami <gbtami@gmail.com>
2022-09-09 13:18:56 +0200

    Change Shinobi Captain and Dragon in start FEN

--- client/chess.ts
+++ client/chess.ts
@@ -686,12 +686,12 @@ export const VARIANTS: { [name: string]: Variant } = {
 
     shinobi: new Variant({
         name: "shinobi", tooltip: () => _("Asymmetric variant which pits the western Chess army against a drop-based, Shogi-styled army."),
-        startFen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/LH1CK1HL[LHMMDJ] w kq - 0 1",
+        startFen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/LH1DK1HL[LHMMCJ] w kq - 0 1",
         board: "standard8x8", piece: "shinobi",
         firstColor: "Pink", secondColor: "Black",
         pieceLetters: ["k", "d", "j", "c", "l", "h", "m", "p"],
         pieceLetters2: ["k", "q", "r", "b", "n", "p"],
-        pocketLetters: ["l", "h", "m", "d", "j"],
+        pocketLetters: ["l", "h", "m", "c", "d", "j"],
         pocketLetters2: [],
         promotion: "shogi",
         promoteableLetters: ["p", "l", "h", "m"],
