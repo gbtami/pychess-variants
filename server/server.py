58ef6e5d599740fc248e6f2722c293b801fc8306
gbtami <gbtami@gmail.com>
2022-10-21 11:07:29 +0200

    Disable test arena run

--- server/server.py
+++ server/server.py
@@ -333,9 +333,9 @@ async def init_state(app):
     # create test tournament
     if 1:
         pass
-        from test_tournament import create_arena_test
+        # from test_tournament import create_arena_test
 
-        await create_arena_test(app)
+        # await create_arena_test(app)
 
         # from test_tournament import create_dev_arena_tournament
         # await create_dev_arena_tournament(app)
