0e023c0ebfac5ac0d930c26c9e07abe3a67d2368
gbtami <gbtami@gmail.com>
2022-11-26 18:08:48 +0100

    Fix Discord bot regressions

--- server/wsl.py
+++ server/wsl.py
@@ -278,13 +278,16 @@ async def lobby_socket_handler(request):
 
                         message = data["message"]
                         response = None
+                        admin_command = False
 
                         if user.username in ADMINS:
                             if message.startswith("/silence"):
+                                admin_command = True
                                 response = silence(message, lobbychat, users)
                                 # silence message was already added to lobbychat in silence()
 
                             elif message.startswith("/stream"):
+                                admin_command = True
                                 parts = message.split()
                                 if len(parts) >= 3:
                                     if parts[1] == "add":
@@ -299,6 +302,7 @@ async def lobby_socket_handler(request):
                                     await broadcast_streams(request.app)
 
                             elif message == "/state":
+                                admin_command = True
                                 server_state(request.app)
 
                             else:
@@ -320,9 +324,10 @@ async def lobby_socket_handler(request):
                         if response is not None:
                             await lobby_broadcast(sockets, response)
 
-                        await request.app["discord"].send_to_discord(
-                            "lobbychat", data["message"], user.username
-                        )
+                        if user.silence == 0 and not admin_command:
+                            await request.app["discord"].send_to_discord(
+                                "lobbychat", data["message"], user.username
+                            )
 
                     elif data["type"] == "logout":
                         await ws.close()
