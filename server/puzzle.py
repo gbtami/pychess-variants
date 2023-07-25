028ad2c8940aa0c55825b5f48a63e1ce429b4b46
gbtami <gbtami@gmail.com>
2023-07-25 11:13:45 +0200

    Fix #1173 Initial mate in x puzzle ratings

--- server/puzzle.py
+++ server/puzzle.py
@@ -39,6 +39,7 @@ def empty_puzzle(variant):
         "fen": sf.start_fen(variant),
         "type": "",
         "moves": "",
+        "eval": "",
     }
     return puzzle
 
@@ -207,8 +208,14 @@ async def update_puzzle_ratings(
 
     wrdiff = int(round(wr.mu - white_rating.mu, 0))
     brdiff = int(round(br.mu - black_rating.mu, 0))
-
     return (wrdiff, brdiff)
+
+
+def default_puzzle_perf(puzzle_eval):
+    perf = DEFAULT_PERF
+    if puzzle_eval[0] == "#":
+        perf["gl"]["r"] = MU + 200 * (int(puzzle_eval[1:]) - 2)
+    return perf
 
 
 class Puzzle:
@@ -216,12 +223,7 @@ class Puzzle:
         self.db = db
         self.puzzle_data = puzzle_data
         self.puzzleId = puzzle_data["_id"]
-        self.perf = puzzle_data.get("perf")
-        if self.perf is None:
-            self.perf = DEFAULT_PERF
-            puzzle_eval = puzzle_data["eval"]
-            if puzzle_eval[0] == "#":
-                self.perf["gl"]["r"] = MU + 200 * (int(puzzle_eval[1:]) - 2)
+        self.perf = puzzle_data.get("perf", default_puzzle_perf(puzzle_data["eval"]))
 
     def get_rating(self, variant: str, chess960: bool) -> Rating:
         gl = self.perf["gl"]
