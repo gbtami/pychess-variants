c6cc3153ba946596acf856ca0d2b31a501531967
gbtami <gbtami@gmail.com>
2023-11-13 14:41:41 +0100

    Fix draw/resign offer regression

--- client/roundCtrl.ts
+++ client/roundCtrl.ts
@@ -317,8 +317,10 @@ export class RoundController extends GameController {
             if (!manualCount)
                 patch(document.getElementById('count') as HTMLElement, h('div'));
             if (this.corr) {
-                (document.getElementById("draw") as HTMLInputElement).disabled = true;
-                (document.getElementById("resign") as HTMLInputElement).disabled = true;
+                const drawEl = document.getElementById("draw") as HTMLInputElement;
+                if (drawEl) drawEl.disabled = true;
+                const resignEl = document.getElementById("resign") as HTMLInputElement;
+                if (resignEl) resignEl.disabled = true;
             }
         } else {
             this.gameControls = patch(container, h('div.btn-controls.game'));
@@ -735,8 +737,10 @@ export class RoundController extends GameController {
             if (container1) patch(container1, h('div#berserk1', ''));
 
             if (!this.spectator && this.corr) {
-                (document.getElementById("draw") as HTMLInputElement).disabled = false;
-                (document.getElementById("resign") as HTMLInputElement).disabled = false;
+                const drawEl = document.getElementById("draw") as HTMLInputElement;
+                if (drawEl) drawEl.disabled = false;
+                const resignEl = document.getElementById("resign") as HTMLInputElement;
+                if (resignEl) resignEl.disabled = false;
             }
         }
 
