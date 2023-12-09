01f37fa50621873f80d1a82a56871a66ce7ebf7d
gbtami <gbtami@gmail.com>
2023-12-09 11:26:11 +0100

    Log Users access errors

--- server/users.py
+++ server/users.py
@@ -1,3 +1,4 @@
+import logging
 from collections import UserDict
 
 from typedefs import db_key, users_key
@@ -5,8 +6,21 @@ from glicko2.glicko2 import DEFAULT_PERF
 from const import ANON_PREFIX, VARIANTS
 from user import User
 
+log = logging.getLogger(__name__)
+
+
+class NotInUsers(Exception):
+    """Raised when dict acces syntax was used, but username not in Users dict"""
+
 
 class Users(UserDict):
+    """
+    The Users class: store user objects in memory
+
+    If we know for sure that username is already in the dict, we can use dictionary access syntax.
+    If not, await get(username) will load user data from mongodb
+    """
+
     def __init__(self, app):
         super().__init__()
         self.app = app
@@ -15,8 +29,7 @@ class Users(UserDict):
         if username in self.data:
             return self.data[username]
         else:
-            print("%s is not in Users. Use await users.get() instead.")
-            raise Exception
+            raise NotInUsers("%s is not in Users. Use await users.get() instead.", username)
 
     async def get(self, username):
         if username in self.data:
@@ -29,7 +42,7 @@ class Users(UserDict):
 
         doc = await self.app[db_key].user.find_one({"_id": username})
         if doc is None:
-            print("--- users.get() %s NOT IN db ---" % username)
+            log.error("--- users.get() %s NOT IN db ---", username)
             return None
         else:
             perfs = doc.get("perfs", {variant: DEFAULT_PERF for variant in VARIANTS})
