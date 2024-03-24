c7fa8341c6e7e350072bd06fcc8914693a4799ad
gbtami <gbtami@gmail.com>
2024-03-24 10:21:55 +0100

    Pieces update

--- client/document.ts
+++ client/document.ts
@@ -108,6 +108,7 @@ export function changePieceCSS(assetUrl: string, family: string, cssFile: string
         case "ataxx": cssLinkIndex += 24; break;
         case "cannonshogi": cssLinkIndex += 25; break;
         case "khans": cssLinkIndex += 26; break;
+        case "dragon": cssLinkIndex += 27; break;
         default: throw "Unknown piece family " + family;
     }
     let newUrl = `${assetUrl}/piece-css/${family}/${cssFile}.css`;
