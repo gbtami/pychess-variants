a392031868de76cd984fe00bb57b3f2d4631e3a8
randompearl-stack <126312812+randompearl-stack@users.noreply.github.com>
2024-03-28 23:36:28 -0700

    Add class name

--- client/lobby.ts
+++ client/lobby.ts
@@ -1084,7 +1084,7 @@ export function lobbyView(model: PyChessModel): VNode[] {
             h('a.reflist', { attrs: { href: 'https://discord.gg/aPs8RKr', rel: "noopener", target: "_blank" } }, 'Discord'),
             h('a.reflist', { attrs: { href: 'https://github.com/gbtami/pychess-variants', rel: "noopener", target: "_blank" } }, 'Github'),
             h('a.reflist', { attrs: { href: 'https://www.youtube.com/channel/UCj_r_FSVXQFLgZLwSeFBE8g', rel: "noopener", target: "_blank" } }, 'YouTube'),
-            h('div', [
+            h('div.internalLinks', [
                 h('a.reflist', { attrs: { href: '/patron' } }, _("Donate")),
                 h('a.reflist', { attrs: { href: '/faq' } }, _("FAQ")),
                 h('a.reflist', { attrs: { href: '/stats' } }, _("Stats")),
