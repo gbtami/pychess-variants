beb9f1bf141f515dfba73f5c6ceea9d41f0a0320
gbtami <gbtami@gmail.com>
2024-07-30 11:54:03 +0200

    Update no puzzle variant lists

--- client/variants.ts
+++ client/variants.ts
@@ -1049,6 +1049,7 @@ export const noPuzzleVariants = [
     "cannonshogi",
     "bughouse",
     "alice",
+    "fogofwar",
 ]
 
 export const variantGroups: { [ key: string ]: { variants: string[] } } = {
