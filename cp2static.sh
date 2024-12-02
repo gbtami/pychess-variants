39c3ac1c719006ec4d731c71b5f4b37dae3a06e0
gbtami <gbtami@gmail.com>
2024-12-03 00:27:55 +0100

    Update cp2static.sh

--- cp2static.sh
+++ cp2static.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cp ./node_modules/ffish-es6/ffish.wasm static/
+cp ./node_modules/ffish-alice-es6/ffish-alice.wasm static/
 cp ./node_modules/fairy-stockfish-nnue.wasm/stockfish.* static/
