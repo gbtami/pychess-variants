2f89172704eb9e1c1e7c1a85db1e1e4dd4c24e70
gbtami <gbtami@gmail.com>
2025-05-11 10:32:13 +0200

    Automate getting latest git tag name

--- md2html.sh
+++ md2html.sh
@@ -4,9 +4,12 @@
 cd static/docs
 node ../../md2html.js
 
+TAGNAME=$(git tag -l -n1 --sort=-v:refname --format='%(refname:short)' | head -n 1)
+echo "${TAGNAME}"
+
 SRC='https://github.com/gbtami/pychess-variants/blob/master'; 
-#DST='https://www.pychess.org';
-DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@1.10.69';
+DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@'$TAGNAME;
+echo "${DST}"
 find . -type f -name "*.html" -exec perl -pi -e s,$SRC,$DST,g '{}' +
 
 mkdir -p ../../templates/docs
