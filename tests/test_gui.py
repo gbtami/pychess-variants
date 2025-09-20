7c83929fd3e8312b830c6431960f5dffcd4e8c73
gbtami <gbtami@gmail.com>
2025-09-20 15:56:38 +0200

    Fix failed editor tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -44,9 +44,10 @@ class TestGUI:
 
             await browser.close()
 
-    @pytest.mark.xfail(reason="Editor tests are currently failing")
     async def test_editor_play_with_machine(self, aiohttp_server):
         app = make_app(db_client=AsyncMongoMockClient())
+        # When tests has redirects port have to be fixed
+        # By default aiohttp_server fixture randomize the port
         server = await aiohttp_server(app, host="127.0.0.1", port=8080)
 
         async with async_playwright() as p:
@@ -58,19 +59,22 @@ class TestGUI:
 
             await page.wait_for_selector(".editor-app", state="visible")
 
-            AI_URL_RE = re.compile(r"http://127\.0\.0\.1:8080/@/Fairy-Stockfish/play/[^/?]+\?fen=.*")
-
             await page.locator("#challengeAI").click()
+
+            AI_URL_RE = re.compile(r"http://127\.0\.0\.1:8080/@/Fairy-Stockfish/play/[^/?]+\?fen=.*")
             await page.wait_for_url(AI_URL_RE)
 
-            await expect(page.locator("div.game-info")).to_be_visible()
+            dialog = page.locator("dialog.modal")
+            await expect(dialog).to_be_visible()
+            await expect(dialog).to_contain_text("Challenge Fairy-Stockfish to a game")
 
             await browser.close()
 
-    @pytest.mark.xfail(reason="Editor tests are currently failing")
     async def test_editor_continue_from_here(self, aiohttp_server):
         app = make_app(db_client=AsyncMongoMockClient())
-        server = await aiohttp_server(app)
+        # When tests has redirects port have to be fixed
+        # By default aiohttp_server fixture randomize the port
+        server = await aiohttp_server(app, host="127.0.0.1", port=8080)
 
         async with async_playwright() as p:
             browser = await p.chromium.launch(headless=True)
@@ -82,6 +86,10 @@ class TestGUI:
             await page.wait_for_selector(".editor-app", state="visible")
 
             await page.locator("#createseek").click()
+
+            SEEK_URL_RE = re.compile(r"http://127\.0\.0\.1:8080/seek/[^/?]+\?fen=.*")
+            await page.wait_for_url(SEEK_URL_RE)
+
             dialog = page.locator("dialog.modal")
             await expect(dialog).to_be_visible()
             await expect(dialog).to_contain_text("Create a game")
