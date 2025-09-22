00b7e4c62226d3caa1b1747ee001ec678d994430
gbtami <gbtami@gmail.com>
2025-09-22 13:49:56 +0200

    Make test_gui pass again

--- client/url.ts
+++ client/url.ts
@@ -2,7 +2,7 @@
 export function sanitizeURL(url: string | null): string {
     try {
         const parsedURL = new URL(url ?? "", window.location.origin);
-        return parsedURL.href;
+        return parsedURL.href.replace(/\/$/, '');
     } catch {
         console.warn("Invalid URL detected, using default safe value.");
         return window.location.origin; // Default safe value
