eb933374e709b55779e0e40576a7f4514640113e
gbtami <gbtami@gmail.com>
2025-11-07 14:30:39 +0100

    Add missing tournament creator data to the base template

--- client/main.ts
+++ client/main.ts
@@ -69,7 +69,7 @@ function initModel(el: HTMLElement) {
         gameId : el.getAttribute("data-gameid") ?? "",
         tournamentId : el.getAttribute("data-tournamentid") ?? "",
         tournamentname : el.getAttribute("data-tournamentname") ?? "",
-        creator: el.getAttribute("data-creator") ?? "",
+        tournamentcreator: el.getAttribute("data-tournamentcreator") ?? "",
         inviter : el.getAttribute("data-inviter") ?? "",
         ply : parseInt(""+el.getAttribute("data-ply")),
         initialFen : el.getAttribute("data-initialfen") ?? "",
