7daa3ca1950a109231fee6afb60999e9dad95308
gbtami <gbtami@gmail.com>
2025-11-13 01:30:02 +0100

    Fix race condition in rematch functionality
    - Added move_lock synchronization to handle_rematch function in wsr.py
    - Added move_lock synchronization to handle_rematch_bughouse function in bug/wsr_bug.py
    - This prevents race conditions when multiple users send rematch requests simultaneously
    - Fixes intermittent failures in test_960_rematch.py where FEN generation was inconsistent

--- server/bug/wsr_bug.py
+++ server/bug/wsr_bug.py
@@ -70,63 +70,65 @@ async def handle_resign_bughouse(data, game: GameBug, user):
 
 
 async def handle_rematch_bughouse(app_state: PychessGlobalAppState, game, user):
-    log.info("rematch request by %s.", user)
-    rematch_id = None
-    other_players = filter(lambda p: p.username != user.username, game.non_bot_players)
+    # Use the game's move_lock to ensure atomic operations for rematch functionality
+    async with game.move_lock:
+        log.info("rematch request by %s.", user)
+        rematch_id = None
+        other_players = filter(lambda p: p.username != user.username, game.non_bot_players)
 
-    log.info("other_plauers %s.", other_players)
-    if all(
-        elem in game.rematch_offers
-        for elem in map(lambda u: app_state.users[u.username].username, other_players)
-    ):
-        color = "w"  # if game.wplayer.username == opp_name else "b"
-        fen = game.initial_fen
+        log.info("other_plauers %s.", other_players)
+        if all(
+            elem in game.rematch_offers
+            for elem in map(lambda u: app_state.users[u.username].username, other_players)
+        ):
+            color = "w"  # if game.wplayer.username == opp_name else "b"
+            fen = game.initial_fen
 
-        reused_fen = True
-        if game.chess960 and game.new_960_fen_needed_for_rematch:
-            fen = FairyBoard.start_fen(game.variant, game.chess960, disabled_fen=game.initial_fen)
-            reused_fen = False
+            reused_fen = True
+            if game.chess960 and game.new_960_fen_needed_for_rematch:
+                fen = FairyBoard.start_fen(game.variant, game.chess960, disabled_fen=game.initial_fen)
+                reused_fen = False
 
-        seek_id = await new_id(None if app_state.db is None else app_state.db.seek)
-        seek = Seek(
-            seek_id,
-            game.bplayer,
-            game.variant,
-            fen=fen,
-            color=color,
-            base=game.base,
-            inc=game.inc,
-            byoyomi_period=game.byoyomi_period,
-            level=game.level,
-            rated=game.rated,
-            player1=game.bplayer,
-            player2=game.wplayer,
-            bugPlayer1=game.wplayerB,
-            bugPlayer2=game.bplayerB,
-            chess960=game.chess960,
-            reused_fen=reused_fen,
-        )
-        app_state.seeks[seek.id] = seek
+            seek_id = await new_id(None if app_state.db is None else app_state.db.seek)
+            seek = Seek(
+                seek_id,
+                game.bplayer,
+                game.variant,
+                fen=fen,
+                color=color,
+                base=game.base,
+                inc=game.inc,
+                byoyomi_period=game.byoyomi_period,
+                level=game.level,
+                rated=game.rated,
+                player1=game.bplayer,
+                player2=game.wplayer,
+                bugPlayer1=game.wplayerB,
+                bugPlayer2=game.bplayerB,
+                chess960=game.chess960,
+                reused_fen=reused_fen,
+            )
+            app_state.seeks[seek.id] = seek
 
-        response = await join_seek_bughouse(
-            app_state, None, seek.id, None, "all-joined-players-set-generate-response"
-        )
-        rematch_id = response["gameId"]
-        for u in set(game.non_bot_players):
-            await u.send_game_message(game.id, response)
-    else:
-        game.rematch_offers.add(user.username)
-        response = {
-            "type": "rematch_offer",
-            "username": user.username,
-            "message": "Rematch offer sent by %s" % user.username,
-            "room": "player",
-            "user": "",
-        }
-        game.messages.append(response)
-        for u in set(game.non_bot_players):
-            await u.send_game_message(game.id, response)
-    if rematch_id:
-        await round_broadcast(game, {"type": "view_rematch", "gameId": rematch_id})
+            response = await join_seek_bughouse(
+                app_state, None, seek.id, None, "all-joined-players-set-generate-response"
+            )
+            rematch_id = response["gameId"]
+            for u in set(game.non_bot_players):
+                await u.send_game_message(game.id, response)
+        else:
+            game.rematch_offers.add(user.username)
+            response = {
+                "type": "rematch_offer",
+                "username": user.username,
+                "message": "Rematch offer sent by %s" % user.username,
+                "room": "player",
+                "user": "",
+            }
+            game.messages.append(response)
+            for u in set(game.non_bot_players):
+                await u.send_game_message(game.id, response)
+        if rematch_id:
+            await round_broadcast(game, {"type": "view_rematch", "gameId": rematch_id})
 
-    return response
+        return response
