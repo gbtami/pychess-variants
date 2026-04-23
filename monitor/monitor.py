2fac2e40705f4291b92f4ae1d0722ac9f7a45061
gbtami <gbtami@gmail.com>
2026-04-23 11:31:10 +0200

    Updated shogi portella intl

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -28,7 +28,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 PYCHESS_MONITOR_TOKEN = os.getenv("PYCHESS_MONITOR_TOKEN", "")
-URL = "http://localhost:8080/metrics"
+# URL = "http://localhost:8080/metrics"
+URL = "https://www.pychess.org/metrics"
 
 
 class MemoryMonitorApp(App):
