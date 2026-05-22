43c6ecd9ce114df4b4806b9fd62d891c65b848a7
gbtami <gbtami@gmail.com>
2026-05-23 00:37:00 +0200

    Add admin/developer profile badges with Lichess-style glyph rendering

--- server/custom_trophy_owners.py
+++ server/custom_trophy_owners.py
@@ -2,6 +2,16 @@ from __future__ import annotations
 
 CUSTOM_TROPHY_OWNERS = {
     # "username": (("variant", "key in TROPHIES"),...)
+    "e-pluszak": (("chess", "admin"), ("chess", "developer")),
+    "BortElGrasitas": (("chess", "admin"),),
+    "okei": (("chess", "admin"),),
+    "visualdennis": (("chess", "admin"), ("chess", "developer")),
+    "furumin999": (("chess", "admin"), ("chess", "developer")),
+    "JarlCarlander": (("chess", "admin"),),
+    "ubdip": (("chess", "admin"), ("chess", "developer")),
+    "CouchTomato87": (("chess", "admin"), ("chess", "developer")),
+    "gbtami": (("chess", "admin"), ("chess", "developer")),
+    "blunderman1": (("chess", "developer"),),
     "arimakat": (
         ("antichess", "acwc19"),
         #        ("3check", "3wc21"),
