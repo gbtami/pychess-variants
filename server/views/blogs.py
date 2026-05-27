7421addb6688c1aef3ec7e944965ef977514de31
gbtami <gbtami@gmail.com>
2026-05-28 00:55:34 +0200

    blogs: keep /blogs official and make post report icon-only

--- server/views/blogs.py
+++ server/views/blogs.py
@@ -1,18 +1,15 @@
 import aiohttp_jinja2
 from aiohttp import web
 
-from ublog import display_date, image_src, post_url, summary_from_markdown
 from utils import get_blogs
 from typing_defs import ViewContext
 from views import get_user_context
-from pychess_global_app_state_utils import get_app_state
 
 
 @aiohttp_jinja2.template("blogs.html")
 async def blogs(request: web.Request) -> ViewContext:
     _user, context = await get_user_context(request)
 
-    app_state = get_app_state(request.app)
     tag = (request.rel_url.query.get("tags") or "").strip().lower()
     blogs = await get_blogs(request, tag=tag, limit=0)
     context["site_tag"] = tag
@@ -26,42 +23,4 @@ async def blogs(request: web.Request) -> ViewContext:
     )
 
     context["blogs"] = blogs
-    context["community_posts"] = []
-    if app_state.db is not None:
-        community_query: dict[str, object] = {
-            "live": True,
-            "$or": [{"blogType": {"$exists": False}}, {"blogType": {"$ne": "site"}}],
-        }
-        posts = await (
-            app_state.db.ublog_post.find(community_query)
-            .sort([("sticky", -1), ("publishedAt", -1), ("createdAt", -1)])
-            .limit(6)
-            .to_list(6)
-        )
-        titles = await app_state.public_users.get_titles(
-            str(post.get("author") or "") for post in posts
-        )
-        context["community_posts"] = [
-            {
-                "_id": post["_id"],
-                "title": str(post.get("title") or ""),
-                "intro": str(post.get("intro") or ""),
-                "subtitle": str(post.get("intro") or ""),
-                "summary": summary_from_markdown(str(post.get("markdown") or "")),
-                "author": str(post.get("author") or ""),
-                "author_title": titles.get(str(post.get("author") or ""), ""),
-                "atitle": titles.get(str(post.get("author") or ""), ""),
-                "date": display_date(post),
-                "image": str(post.get("image") or ""),
-                "image_src": image_src(post),
-                "imageAlt": str(post.get("imageAlt") or ""),
-                "alt": str(post.get("imageAlt") or ""),
-                "url": post_url(post),
-                "tags": [],
-                "topics": [str(topic) for topic in post.get("topics", [])],
-                "isOfficial": False,
-                "blogType": "community",
-            }
-            for post in posts
-        ]
     return context
