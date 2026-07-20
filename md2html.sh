963dee44c211cf0f2939c1959f6289a9ba56e38a
gbtami <gbtami@gmail.com>
2026-07-20 16:29:00 +0200

    Bump asset version

--- md2html.sh
+++ md2html.sh
@@ -9,7 +9,7 @@ echo "${TAGNAME}"
 
 SRC='https://github.com/gbtami/pychess-variants/blob/master';
 # DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@'$TAGNAME;
-DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@1.11.36';
+DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@1.11.37';
 echo "${DST}"
 find . -type f -name "*.html" -exec perl -pi -e s,$SRC,$DST,g '{}' +
 
