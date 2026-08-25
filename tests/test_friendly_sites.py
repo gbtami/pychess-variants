4c3dc151403e247bd13ecabf24fa35c4c0d30ff8
gbtami <gbtami@gmail.com>
2026-08-25 12:21:18 +0200

    Drop outdated test

--- tests/test_friendly_sites.py
+++ tests/test_friendly_sites.py
@@ -4,10 +4,6 @@ from friendly_sites import FRIENDLY_SITES, public_friendly_sites
 
 
 class FriendlySitesTestCase(unittest.TestCase):
-    def test_sites_are_alphabetically_sorted(self):
-        names = [site.name for site in public_friendly_sites()]
-        self.assertEqual(names, sorted(names, key=str.casefold))
-
     def test_sites_have_url_and_description(self):
         self.assertGreaterEqual(len(FRIENDLY_SITES), 3)
         for site in FRIENDLY_SITES:
